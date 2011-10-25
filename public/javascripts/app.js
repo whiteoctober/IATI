41650a7546915d8d7763001180ddee8c1a7bdb48
Peter West <pete@whiteoctober.co.uk>
2011-10-25 15:56:14 +0100

    Removed bubble layout padding to center and support zooming

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,5 @@
 (function() {
+
   //Colour palette for activity bubbles
   var palette = [
     {colour: '#3366FF', text: '#fff'},
@@ -9,12 +10,15 @@
     {colour: '#003DF5', text: '#fff'},
     {colour: '#002EB8', text: '#fff'}
   ];
+  //Maximum and minimum font sizes for activity bubbles
   var fontRange = {min: 11, max: 30};
+  var bubbleRadiusRange = {min: 50, max: 180};
   var popup = $("#popup");
   var content = $("#content");
   var query = window.location.search;
 
-  var filterUpdated = function(html) { 
+  //Callback for when a filter has been submitted
+  var filterSubmitted = function(html) { 
     $("#content_inner").html(html);
     popup.addClass("hidden");
     $("a.filter").each(function() {
@@ -23,6 +27,7 @@
     $(".activities").each(redrawActivities);
   };
 
+  //Callback for when a filter is opened
   var filterLoaded = function(html) { 
     popup.html(html);
     popup.removeClass("hidden");
@@ -33,7 +38,7 @@
         query = popup.find("form.filter").serialize();
       },
       dataType: 'html',
-      success: filterUpdated
+      success: filterSubmitted
     });
   };
 
@@ -49,6 +54,8 @@
   
   var randomly = function(a,b) { return Math.random() * 2 - 1; };
   var isLeafNode = function(d) { return !d.children; };
+  
+  //Finds a sector index based on angle from the centre
   var sliceIndex = function(position, area, slices) {
     var delta = {
       x: area.x / 2 - position.x,
@@ -57,6 +64,7 @@
     return parseInt(slices * (Math.atan2(delta.x, delta.y) / Math.PI + 1)/2) % slices;
   };
   
+  //Scales text within an element
   var scaleText = function(element, text) {
     var words = (text || "").split(" ");
     var length = words.length;
@@ -79,10 +87,7 @@
   var redrawActivities = function() {
     var activities = $(this);
     var frameHeight = Math.max(window.innerHeight, $("#content").height());
-    var area = {
-      x: $("#content").width(),
-      y: 600 //frameHeight - $("#header").height() - $("#footer").height()
-    };
+    var area = { x: 600, y: 600 };
     
     var data = activities.children().map(function() {
       var activity = $(this);
@@ -95,11 +100,29 @@
     
     var positions = {};
     var bubble = d3.layout.pack().sort(randomly).size([area.x, area.y]);
+    var coords = {top: [], bottom: [], left: [], right: []};
     $.map(bubble.nodes({children: data}).filter(isLeafNode), function(position) {
       positions[position.id] = position;
       position.x = parseInt(position.x);
       position.y = parseInt(position.y);
-      position.radius = parseInt(position.r);
+      position.radius = Math.min(parseInt(position.r), bubbleRadiusRange.max);
+      coords.left.push(position.x - position.radius);
+      coords.top.push(position.y - position.radius);
+      coords.right.push(position.x + position.radius);
+      coords.bottom.push(position.y + position.radius);
+    });
+    
+    var offset = {
+      top: Math.min.apply(this, coords.top),
+      left: Math.min.apply(this, coords.left),
+      right: Math.max.apply(this, coords.right),
+      bottom: Math.max.apply(this, coords.bottom)
+    };
+    area = {x: offset.right - offset.left, y: offset.bottom - offset.top };
+    
+    $.each(positions, function(id, position) {
+      positions[id].x =  position.x - offset.left;
+      positions[id].y =  position.y - offset.top;
     });
     
     activities.css({width: area.x, height: area.y});
@@ -107,12 +130,13 @@
       var activity = $(this);
       var position = positions[activity.attr("id")];
       
-      var colourIndex = sliceIndex(position, {x: area.x - position.radius, y: area.y - position.radius}, palette.length);
+      var centre = {x: area.x - position.radius, y: area.y - position.radius};
+      var colourIndex = sliceIndex(position, centre, palette.length);
       var colours = palette[colourIndex || 0];
       activity.css({
         position: 'absolute',
-        top: position.y - position.radius, 
         left: position.x - position.radius,
+        top: position.y - position.radius, 
         width: position.radius * 2,
         height: position.radius * 2
       });
@@ -122,7 +146,7 @@
       var text = activity.find(".text");
       text.css({ 
         color: colours.text, 
-        margin: parseInt(position.radius * 0.57 / 2)
+        margin: parseInt(position.radius * 0.6 / 2)
       });
       activities.children().removeClass("hidden");
       scaleText(text, position.name);
