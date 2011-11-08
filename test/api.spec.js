d42e1ef8cfa5d88a51f9c45ddd56640008e983b9
Ben Foxall <benfoxall@gmail.com>
2011-11-08 19:19:45 +0000

    added some tests to the api wrapper spec

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,5 +1,9 @@
-var gently = global.GENTLY = new (require('gently'))();
-var api = require('../lib/api.js');
+var gently = global.GENTLY = new (require('gently'))(),
+    api = require('../lib/api.js'),
+    url = require('url'),
+    http = require('http');
+    
+
 
 describe("request", function() {
   
@@ -39,5 +43,58 @@ describe("request", function() {
     
   });
   
+  it('should pass params to the request', function(){
+    
+    var req = new api.Request({myParam:'1', another:'2'});
+    
+    gently.expect(gently.hijacked.request, 'get', 1, function(params, callback) {
+      var query = url.parse(params.uri, true).query;
+      
+      expect(query.myParam).toBe('1');
+      expect(query.another).toBe('2');
+      expect(query.notanother).toBeUndefined();
+      
+      callback(null, { statusCode: 200 },  '{}');
+    });
+    
+    req.end();
+  });
+  
+  
+});
+
+
+describe("errors", function(){
+  
+  it('emits errors on non-json data', function(){
+    
+    gently.expect(gently.hijacked.request, 'get', function(params, callback) {
+       callback(null, { statusCode: 200 },  'not-json');
+    });
+    
+    var re = api.Request({});
+    
+    gently.expect(re, 'emit', function(event) {
+      expect(event).toBe('error');
+    });
+    
+    re.end();
+    
+  });
+  
+  it('emits error on timeout', function(){
+    
+    gently.expect(gently.hijacked.request, 'get', function(params, callback) {
+       callback({message:'ETIMEDOUT', code:'ETIMEDOUT'});
+    });
+
+    var re = api.Request({});
+    gently.expect(re, 'emit', function(event) {
+      expect(event).toBe('error');
+    });
+    
+    re.end();
+    
+  });
   
 });
