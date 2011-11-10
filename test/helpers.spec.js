5c43b8bc50baa001e8d35170d2bfd894711d642c
Ben Foxall <benfoxall@gmail.com>
2011-11-10 15:47:25 +0000

    added helpers lib

---
+++ test/helpers.spec.js
@@ -0,0 +1,14 @@
+var gently = global.GENTLY = new (require('gently'))(),
+    helpers = require('../lib/helpers.js');
+
+describe('currency helper', function(){
+  
+  it('works', function(){
+    expect(helpers.to_currency('1')).toBe('$1');
+    expect(helpers.to_currency('1234')).toBe('$1,234');
+    expect(helpers.to_currency('1E3')).toBe('$1,000');
+    expect(helpers.to_currency('1.234567E3')).toBe('$1,234');
+    expect(helpers.to_currency('1000000000')).toBe('$1,000,000,000');
+  });
+  
+});
