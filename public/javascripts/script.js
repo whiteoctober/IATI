98a42d6fe57e6fabf25c695d3ec1d54f185a4bfa
Ben Foxall <benfoxall@gmail.com>
2011-11-22 13:51:32 +0000

    fix: dialog hiding on iPad

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -36,7 +36,7 @@ var bubbleClasses = 'c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12 c13'.split(' ');
     e.preventDefault();
   });
   
-  $('#dialog').live('click', function() {
+  $('#dialog').live('touchstart click', function() {
     $(this).fadeOut(function() {
       $(this).remove();
     });
