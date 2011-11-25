89e0a900b6e76118969d402099f074379fa0d023
Ben Foxall <benfoxall@gmail.com>
2011-11-25 19:28:01 +0000

    fixed dialog close error

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -36,7 +36,10 @@ var bubbleClasses = 'c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12 c13'.split(' ');
     e.preventDefault();
   });
   
-  $('#dialog').live('touchstart click', function() {
+  $('#dialog').live('touchstart click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
     $(this).fadeOut(function() {
       $(this).remove();
     });
