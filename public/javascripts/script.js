326934e2a84dc819ea89731e7afc7888af43eb27
Ben Foxall <benfoxall@gmail.com>
2011-11-25 19:47:53 +0000

    display embed iframes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -30,6 +30,8 @@ var bubbleClasses = 'c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12 c13'.split(' ');
           return;
         }
         embed.removeClass("hidden");
+        // console.log(this);
+        runInlines.apply(this);
       });
       dimmed = true; 
     });
