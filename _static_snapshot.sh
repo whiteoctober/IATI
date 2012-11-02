12f6e7fd9e30372332c0a3cb78951f04af026674
Ben Foxall <benfoxall@gmail.com>
2012-11-02 11:35:20 +0000

    datafile now included in snapshot

--- _static_snapshot.sh
+++ _static_snapshot.sh
@@ -4,6 +4,7 @@ rm -r localhost\:3000
 
 wget --html-extension -p -k http://localhost:3000/
 wget --html-extension -p -k http://localhost:3000/filter/SectorCategory
+wget --html-extension -p -k http://localhost:3000/data-file?Funder=GB&Country=AF&view=false
 wget --html-extension -p -k http://localhost:3000/activities?SectorCategory=311
 wget --html-extension -p -k http://localhost:3000/activity/GB-CHC-285776-ZIM349
 
@@ -15,10 +16,14 @@ cp -r public/images localhost\:3000/images
 
 # update stylesheets to use relative images
 find localhost\:3000/stylesheets/*.css -exec sed -i remove 's/http:\/\/localhost:3000/../g' {} \;
-
 rm localhost\:3000/stylesheets/*.cssremove
 
-rm localhost\:3000/stylesheets/robots.txt
+# change the iframes to point to a particular ip address
+#find localhost\:3000/*.html -exec sed -i remove 's/localhost:3000/192\.168\.1\.132:3000/g' {} \;
+#find localhost\:3000/activity/*.html -exec sed -i remove 's/localhost:3000/192\.168\.1\.132:3000/g' {} \;
+
+rm localhost\:3000/*.htmlremove
+rm localhost\:3000/activity/*.htmlremove
 
 
 mv public/robots.x public/robots.txt
